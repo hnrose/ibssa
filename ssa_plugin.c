8760408ec123ae74730cbf34ab617b8fc85dfd1c
Hal Rosenstock <hal@mellanox.com>
2013-07-15 20:43:58 +0300

    ssa_plugin.c: Use C99 syntax for initialization
    Signed-off-by: Hal Rosenstock <hal@mellanox.com>

--- ssa_plugin.c
+++ ssa_plugin.c
@@ -344,8 +344,8 @@ static void report(void *_ssa, osm_epi_event_id_t event_id, void *event_data)
 #endif
 
 osm_event_plugin_t osm_event_plugin = {
-      osm_version:OSM_VERSION,
-      create:construct,
-      delete:destroy,
-      report:report
+      .osm_version = OSM_VERSION,
+      .create = construct,
+      .delete = destroy,
+      .report = report
 };
